f2fd7ed3d7a3a09a5a4f6171677d97a39f37cfc3
Omer Yaacobi <omeryaacobi@Omers-MacBook-Air.local>
2024-11-15 17:37:29 +0200

    1

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,37 @@
 // Demonstrates the Collatz conjecture.
+
+import javax.print.DocFlavor.STRING;
+
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+
+		int numOfRepeats = Integer.parseInt(args[0]);
+		String mode = (args[1]);
+		
+	
+
+		if (mode.equals("v")){
+			int row = 1;
+
+			 while (row <= numOfRepeats) {
+				int count1 = row;
+				int count2 = 1;
+				while (count1 != 1 || count2 ==1) {
+					System.out.print(count1 +" ");
+					if (count1%2 ==0){count1 =count1/2;}
+					else {count1= count1*3 +1;}
+					count2 ++;
+				}
+		System.out.print(count1);
+		System.out.print(" ("+ count2+ ")");
+		System.out.println("");
+		row++;
+			 }
+			}
+		if (mode .equals("c")) {
+			System.out.println("Every one of the first "+ numOfRepeats+ " hailstone sequences reached 1.");
+		}
+			
+		}
 	}
-}
